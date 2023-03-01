e2fb3407376702b282b69b55ed190c81e52b032f
Rizky Naufal <naufalrizky79@gmail.com>
2023-03-02 00:30:19 +0700

    init

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ http.createServer(function(req, res) {
     var filename = "." + q.pathname;
     fs.readFile(filename, function(err, data) {
         if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.writeHead(404, { 'Content-Type': '/html' });
             return res.end("404 Not Found");
         }
         res.writeHead(200, { 'Content-Type': 'text/html' });
