2fe1437790d9c6233fe8e95a9cc758a01325cc7e
xikdp <dandakomang09@gmail.com>
2023-03-02 19:39:11 +0700

    inisial project be fe

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,11 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+const express = require('express'); //memanggil express
 
-http.createServer(function(req, res) {
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
-    fs.readFile(filename, function(err, data) {
-        if (err) {
-            res.writeHead(404, { 'Content-Type': '/html' });
-            return res.end("404 Not Found");
-        }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
+
+const app = express(); //memanggil method express
+
+app.use(() => {
+    console.log("hello hallodeck");
+    console.log("hello world");
+})
+
+app.listen(8888);
