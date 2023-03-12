2befe2a0c56269af14384a43b9cbe29bd7a1dbdd
xikdp <dandakomang09@gmail.com>
2023-03-12 23:28:26 +0700

    Update Customercontoller

--- backend/controllers/Customercontroller.js
+++ backend/controllers/Customercontroller.js
@@ -1,11 +1,11 @@
 const crypto = require("crypto");
-const id = crypto.randomBytes(4).toString("hex");
+const id = crypto.randomBytes(3).toString("hex");
 const name = "usr";
 
 
 exports.custLogReg = (req, res, next) => {
     res.json ({
         message: "berhasil membuat unique id",
-        data: [name, id]
+        data: [name+"-"+id]
     })
 };
