07353642f94731b9f45b377953ced4c61fd3de2b
xikdp <dandakomang09@gmail.com>
2023-03-12 16:47:23 +0700

    test unique id for customer id resgiter/login

---
+++ backend/model/Customermodel.js
@@ -0,0 +1,14 @@
+// require("../config/database");
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+
+
+
+// const CustomerModel = mongoose.Schema({
+//     namaCust: {
+//         type: String,
+//         required: true
+//     },
+//     idCust: 
+// })
