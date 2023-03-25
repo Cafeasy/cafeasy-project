7a771e663e4d2444a28bf572931814e724bd271e
muhamadadam2001 <61117916+muhamadadam2001@users.noreply.github.com>
2023-03-25 19:11:06 +0700

    update

--- frontend/src/Components/Detailmenucomp.js
+++ frontend/src/Components/Detailmenucomp.js
@@ -1,22 +1,9 @@
+import "../Style/Detailmenupage.css";
 import * as React from "react"
 import Gambarburger from '../Photo/Burger.jpeg'
 import { useEffect, useState } from "react"
-import axios from "axios";
 
 export default function App() {
-    const user = props.user;
-    const [menus, setMenus] = useState([]);
-    useEffect(() => {
-        axios.get('http://localhost:8888/ListMenu')
-            .then(result => {
-                console.log('data API ada', result.data);
-                const responseAPI = result.data;
-                setMenus(responseAPI.data);
-            })
-            .catch(err => {
-                console.log('error: data tidak terambil - ', err);
-            })
-    })
   const [offset, setOffset] = useState(0)
 
   useEffect(() => {
@@ -40,7 +27,7 @@ export default function App() {
           className="parallax"
         />
         <div className="text-wrapper">
-          <h1 className="headline">{menu.namaMenu}</h1>
+          <h1 className="headline">Parallax</h1>
           <h2 className="sub-headline">Scrolling effect</h2>
         </div>
       </section>
