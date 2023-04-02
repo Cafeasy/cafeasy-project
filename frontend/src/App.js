40ee53cc02c3dfef4896bf13780d18d5a2b5094c
adjierizqan <79629442+adjierizqann@users.noreply.github.com>
2023-04-02 17:54:01 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,7 @@ function App() {
         <Route exact path='/Navbarpage' element={<Navbarpage />} />
         <Route exact path='/Berandapage/:idUser' element={<Berandapage />} />
         <Route exact path='/ListMenu' element={<ListMenupage />} />
-        <Route exact path='/KonfimasiPesanan' element={<Confirmpage />} />
+        <Route exact path='/KonfirmasiPesanan/:idUser' element={<Confirmpage />} />
         <Route exact path='/MetodePembayaran' element={<Methodpage />} />
         <Route exact path='/Detailmenu/:idMenu' element={<Detailmenupage />} />
       </Routes>
