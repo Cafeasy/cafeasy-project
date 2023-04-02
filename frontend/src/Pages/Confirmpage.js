d22dffa115d6ec80a728e48bde418f6ef28f2624
adjierizqan <79629442+adjierizqann@users.noreply.github.com>
2023-04-02 17:55:38 +0700

    update route confirm page

--- frontend/src/Pages/Confirmpage.js
+++ frontend/src/Pages/Confirmpage.js
@@ -1,8 +1,40 @@
 import Confirmcomp from "../Components/Confirmcomp"
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const Confirmpage = () => {
+import { useParams } from "react-router-dom";
+
+function Confirmpage(props) {
+    const [menu, setMenu] = useState([]);
+    const idUser = props.idUser;
+
+    const idMenu = useParams();
+    const url = `${process.env.REACT_APP_API_URL}/KonfirmasiPesanan/${idMenu.idMenu}`;
+
+    useEffect(() => {
+        axios
+            .get(url)
+            .then((result) => {
+
+
+                const responseAPI = result.data;
+                const data = responseAPI.data;
+                data.map((menus) => {
+                    setMenu(menus);
+                })
+
+
+                console.log("data API ada", result.data);
+            })
+            .catch((err) => {
+                console.log("Data tidak berhasil dipanggil");
+            });
+    }, []);
+
     return (
-        <Confirmcomp />
+
+
+        <Confirmcomp menu={menu} idUser={idUser} />
     )
 }
 
