82368a08535039e7b5fb2cc4914ac893a3647cf0
xikdp <dandakomang09@gmail.com>
2023-06-06 16:51:55 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://hallodeck:hallodeck5@ac-fyxbv6h-shard-00-00.i4jusgn.mongodb.net:27017,ac-fyxbv6h-shard-00-01.i4jusgn.mongodb.net:27017,ac-fyxbv6h-shard-00-02.i4jusgn.mongodb.net:27017/cafeasy?ssl=true&replicaSet=atlas-9r1c1k-shard-0&authSource=admin&retryWrites=true&w=majority',{
+mongoose.connect(process.env.MONGO_SERVER_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
