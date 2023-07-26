47a792b1c4b4d2457003780252fdd7598015c028
rizkynfal <naufalrizky79@gmail.com>
2023-07-26 14:35:16 +0700

    update

--- backend/controllers/TransaksiCustomercontroller.js
+++ backend/controllers/TransaksiCustomercontroller.js
@@ -53,57 +53,62 @@ exports.getDetailTransaksiPelanggan = async (req, res, next) => {
 exports.postTransaksiPelanggan = async (req, res, next) => {
     //check data berdasarkan request idKeranjang req url
     var idKeranjangCheck = req.params.idKeranjang;
-
+    let checkKeranjangByParams;
     //query cari data keranjang berdasarkan id yang direquest pada url dan menjadikan object
-    let checkKeranjangByParams = await KeranjangPelanggan.findOne({ idKeranjang: `${idKeranjangCheck}` });
-    let obyekKeranjang = checkKeranjangByParams.toObject();
+    try {
+        checkKeranjangByParams = await KeranjangPelanggan.findOne({ idKeranjang: `${idKeranjangCheck}` });
 
-    //sign data ke variable yang akan dijadikan data insert keranjang
-    var idKeranjangSign = obyekKeranjang.idKeranjang;
-    var idPelangganSign = obyekKeranjang.idPelanggan;
-    var namaPelangganSign = obyekKeranjang.namaPelanggan;
-    var dataPesananSign = obyekKeranjang.dataPesanan;
-    var noMejaPelanggan = obyekKeranjang.noMeja;
+        let obyekKeranjang = checkKeranjangByParams.toObject();
+        //sign data ke variable yang akan dijadikan data insert keranjang
+        var idKeranjangSign = obyekKeranjang.idKeranjang;
+        var idPelangganSign = obyekKeranjang.idPelanggan;
+        var namaPelangganSign = obyekKeranjang.namaPelanggan;
+        var dataPesananSign = obyekKeranjang.dataPesanan;
+        var noMejaPelanggan = obyekKeranjang.noMeja;
 
-    //date gmt
-    var ndate = new Date().toLocaleString('en-US', {
-        timeZone: 'Asia/Jakarta'
-    })
+        //date gmt
+        var ndate = new Date().toLocaleString('en-US', {
+            timeZone: 'Asia/Jakarta'
+        })
 
-    //var untuk menampung total harga
-    var totalHarga = 0;
+        //var untuk menampung total harga
+        var totalHarga = 0;
 
-    //looping untuk mengambil total harga data pesanan
-    const len = obyekKeranjang.dataPesanan.length;
-    // console.log(obyekKeranjang.dataPesanan);
-    for (var i = 0; i < len; i++) {
-        totalHarga = totalHarga + (obyekKeranjang.dataPesanan[i].hargaMenu * obyekKeranjang.dataPesanan[i].qty)
+        //looping untuk mengambil total harga data pesanan
+        const len = obyekKeranjang.dataPesanan.length;
+        // console.log(obyekKeranjang.dataPesanan);
+        for (var i = 0; i < len; i++) {
+            totalHarga = totalHarga + (obyekKeranjang.dataPesanan[i].hargaMenu * obyekKeranjang.dataPesanan[i].qty)
+        }
+
+        const insertTransaksi = new TransaksiPelanggan({
+            idTransaksi: idKeranjangSign,
+            idPelanggan: idPelangganSign,
+            namaPelanggan: namaPelangganSign,
+            tanggal: ndate,
+            noMeja: noMejaPelanggan,
+            dataPesanan: dataPesananSign,
+            totalHarga: totalHarga,
+            statusBayar: "Belum bayar"
+        })
+
+        insertTransaksi.save().then(result => {
+            res.status(200).json({
+                message: "Transaksi disimpan, selesaikan pembayaran agar pesanan diproses ya",
+                data: result
+            })
+        }).catch(err => {
+            next(err);
+        });
+
+        KeranjangPelanggan.deleteOne(({ idKeranjang: `${idKeranjangCheck}` }))
+            .catch(err => {
+                next(err);
+            })
+    } catch (err) {
+        res.status(401).send({ message: "error", data: err });
     }
 
-    const insertTransaksi = new TransaksiPelanggan({
-        idTransaksi: idKeranjangSign,
-        idPelanggan: idPelangganSign,
-        namaPelanggan: namaPelangganSign,
-        tanggal: ndate,
-        noMeja: noMejaPelanggan,
-        dataPesanan: dataPesananSign,
-        totalHarga: totalHarga,
-        statusBayar: "Belum bayar"
-    })
-
-    insertTransaksi.save().then(result => {
-        res.status(200).json({
-            message: "Transaksi disimpan, selesaikan pembayaran agar pesanan diproses ya",
-            data: result
-        })
-    }).catch(err => {
-        next(err);
-    });
-
-    KeranjangPelanggan.deleteOne(({ idKeranjang: `${idKeranjangCheck}` }))
-        .catch(err => {
-            next(err);
-        })
 }
 
 exports.updateStatusBayar = async (req, res, next) => {
@@ -124,29 +129,29 @@ exports.updateStatusBayar = async (req, res, next) => {
         newStatusBayar = "SUCCESS";
 
         //mendefinisi var length looping untuk update otomatis stok menu
-        var checkTransaksi = await TransaksiPelanggan.findOne({idTransaksi: `${idTransaksiCheck}`});
+        var checkTransaksi = await TransaksiPelanggan.findOne({ idTransaksi: `${idTransaksiCheck}` });
         var obyekTransaksi = checkTransaksi.toObject();
         var len = obyekTransaksi.dataPesanan.length;
 
         //looping update (stok menu - qty datapesanan)
         for (var i = 0; i < len; i++) {
             //menyimpan setiap id menu pada data pesanan ke sebuah variable
-            var checkIdMenuDataPesanan = await TransaksiPelanggan.findOne({idTransaksi: `${idTransaksiCheck}`});
+            var checkIdMenuDataPesanan = await TransaksiPelanggan.findOne({ idTransaksi: `${idTransaksiCheck}` });
             var obyekIdMenu = checkIdMenuDataPesanan.toObject();
             var saveIdMenu = obyekIdMenu.dataPesanan[i].idMenu;
 
             //menyimpan data setiap stok menu per id menu ke sebuah variable 
-            var saveStokMenu = await Menu.findOne({idMenu: `${saveIdMenu}`});
+            var saveStokMenu = await Menu.findOne({ idMenu: `${saveIdMenu}` });
             var obyekStokMenu = saveStokMenu.toObject();
 
             //otomatis update stok menu setelah transaksi berhasil
-            var kalkulasiStokMenu = obyekStokMenu.stokMenu-obyekIdMenu.dataPesanan[i].qty;
+            var kalkulasiStokMenu = obyekStokMenu.stokMenu - obyekIdMenu.dataPesanan[i].qty;
 
             await Menu.findOneAndUpdate(
-                {idMenu: `${saveIdMenu}`},
-                { $set: {stokMenu: kalkulasiStokMenu}},
-                {new: true}
-                )
+                { idMenu: `${saveIdMenu}` },
+                { $set: { stokMenu: kalkulasiStokMenu } },
+                { new: true }
+            )
         }
 
     } else {
